14eb084c4b6ada9a028b310d5886224bf2f149fd
neosu <st2udio@gmail.com>
2022-09-06 11:06:44 +0800

    add gomod test

--- utils/gomod/gomod_test.go
+++ utils/gomod/gomod_test.go
@@ -42,3 +42,77 @@ func TestGetMod(t *testing.T) {
 		})
 	}
 }
+
+func TestGoModule_IsInGoMod(t *testing.T) {
+	type fields struct {
+		Path      string
+		Main      bool
+		Dir       string
+		GoMod     string
+		GoVersion string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "check is in go mod",
+			fields: fields{
+				Path:      "github.com/metagogs/gogs",
+				Main:      true,
+				Dir:       "/Users/neo/Documents/gogs",
+				GoMod:     "/Users/neo/Documents/gogs/go.mod",
+				GoVersion: "1.19",
+			},
+			want: true,
+		},
+		{
+			name: "check is not in go mod",
+			fields: fields{
+				Path:      "command-line-arguments",
+				Main:      true,
+				Dir:       "/Users/neo/Documents/gogs",
+				GoMod:     "/Users/neo/Documents/gogs/go.mod",
+				GoVersion: "1.19",
+			},
+			want: false,
+		},
+		{
+			name: "check is not in go mod",
+			fields: fields{
+				Path:      "",
+				Main:      true,
+				Dir:       "/Users/neo/Documents/gogs",
+				GoMod:     "/Users/neo/Documents/gogs/go.mod",
+				GoVersion: "1.19",
+			},
+			want: false,
+		},
+		{
+			name: "check is not in go mod",
+			fields: fields{
+				Path:      "github.com/metagogs/gogs",
+				Main:      true,
+				Dir:       "/Users/neo/Documents/gogs",
+				GoMod:     "",
+				GoVersion: "1.19",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &GoModule{
+				Path:      tt.fields.Path,
+				Main:      tt.fields.Main,
+				Dir:       tt.fields.Dir,
+				GoMod:     tt.fields.GoMod,
+				GoVersion: tt.fields.GoVersion,
+			}
+			if got := g.IsInGoMod(); got != tt.want {
+				t.Errorf("GoModule.IsInGoMod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
